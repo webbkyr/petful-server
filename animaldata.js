99dc8e0083b84e6774fcee458c9550b83dd6635a
Kayla Webb <kaylarwebb@gmail.com>
2018-02-02 10:58:39 -0500

    Moved animal data into an array and to a separate file

---
+++ animaldata.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const animalData = [
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  }  
+];
+
+module.exports = animalData;
--- index.js
+++ index.js
@@ -8,27 +8,8 @@ const {PORT, CLIENT_ORIGIN} = require('./config');
 const {dbConnect} = require('./db-mongoose');
 // const {dbConnect} = require('./db-knex');
 
+const animalData = require('./animaldata');
 const app = express();
-
-const catData = {
-  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg',
-  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-  name: 'Fluffy',
-  sex: 'Female',
-  age: 2,
-  breed: 'Bengal',
-  story: 'Thrown on the street'
-};
-
-const dogData = {
-  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
-  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
-  name: 'Zeus',
-  sex: 'Male',
-  age: 3,
-  breed: 'Golden Retriever',
-  story: 'Owner Passed away'
-};
 
 app.use(
   morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
@@ -43,11 +24,11 @@ app.use(
 );
 
 app.get('/api/cat', (req, res) => {
-  res.status(200).json(catData);
+  res.json(animalData[0]);
 });
 
 app.get('/api/dog', (req, res) => {
-  res.status(200).json(dogData);
+  res.json(animalData[1]);
 });
 
 
